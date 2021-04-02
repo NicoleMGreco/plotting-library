a59e099d12bc779f0dd3ac9063e2f778d7cee8af
Nicole Greco <nicolemariegreco@gmail.com>
2021-04-02 18:34:36 -0400

    assignment 8

--- plotting2.py
+++ plotting2.py
@@ -5,42 +5,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os as os
+import sys as sys
 
 # Create a function to read the data file
 def read_data(filename,delimiter=',',starting_row=0):
     """This function reads data from a specified filename. 
     The specified filename should point to a .csv file."""
     # Create an array (a multi-dimensional table) out of our data file, full of text
-    all_data = np.genfromtxt("gs_data.csv", delimiter=',',skip_header=1)
-    print(all_data)
+    all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=0)
 
     # Select the data range we are interested in, convert it into a new array, full of numbers
     grainsize_data = np.array(all_data[1:,:], dtype=float)
     return grainsize_data
 
-grainsize_data = read_data("gs_data.csv", starting_row=2)
+def process_data(grainsize_data):
+    # Compute a new column by multiplying column number 1 to Kelvin
+    grainsize_calculated = (grainsize_data[:,0,None] - 5) * 2
 
-# Compute a new column by multiplying column number 1 to Kelvin
-grainsize_calculated = (grainsize_data[:,0,None] - 5) * 2
+    # Append this new column to the existing temperature_data array
+    processed_grainsize_data = np.append(grainsize_data, grainsize_calculated,1)
+    return processed_grainsize_data
 
-# Append this new column to the existing temperature_data array
-processed_grainsize_data = np.append(grainsize_data, grainsize_calculated,1)
-print (processed_grainsize_data)
+def plot_data(processed_grainsize_data, plot_filename):
+    # Create a figure of the processed data
+    grainsize_figure = plt.figure()
+    grainsize_plot = plt.plot (processed_grainsize_data[:,2],processed_grainsize_data[:,1])
 
-# Create a figure of the processed data
-grainsize_figure = plt.figure()
-grainsize_plot = plt.plot (processed_grainsize_data[:,2],processed_grainsize_data[:,1])
-plt.xlabel("Diameter(microns)")
-plt.ylabel("Concentration")
-plt.title("Grain Size Data")
-grainsize_figure.savefig('results/grainsize-fig.png')
+    plt.xlabel("Diameter(microns)")
+    plt.ylabel("Concentration")
+    plt.title("Grain Size Data")
 
+    plt.show(block=True)
+    grainsize_figure.savefig(plot_filename)
 
-#all_data = pd.read_csv("gs_data.csv", index_col='Date', header=4)
-#all_data.info()
-#all_data.to_json("results/data_output.json")
+def convert_data(filename, output_filename):
+    all_data = pd.read_csv(filename, header=0)
+    all_data.info()
+    all_data.to_json(output_filename)
+    json_data = pd.read_json(output_filename)
+    json_data.info()
 
-#json_data = pd.read_json("results/data_output.json")
-#json_data.info()
+def plot():
+    input_file = "gs_data.csv"
+    plot_file = "grainsize-fig.png"
+    json_output_file = "data_output2.json"
 
-#print(json_data.loc['195012':'197512','Value'])
+    data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__), "..","data"))
+    results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__), "..","results"))
+    
+    input_filename = os.path.join(data_directory, input_file)
+    plot_filename = os.path.join(results_directory, plot_file)
+    json_output_file = os.path.join(results_directory, json_output_file)
+
+    grainsize_data = read_data(input_filename, starting_row=0)
+    processed_grainsize_data = process_data(grainsize_data)
+    plot_data(processed_grainsize_data, plot_filename)
+    convert_data(input_filename, json_output_file)
+
+if __name__ == "__main__":
+    print(sys.argv)
+    plot()
