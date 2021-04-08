7b9eac91bff2e7a03b2fb15240308eb41f313354
Nicole Greco <nicolemariegreco@gmail.com>
2021-04-08 14:20:26 -0400

    tests_assignment_9

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,17 +1,37 @@
-import sys, os
+import plotting2 as plotting
+import sys as sys
+import os as os
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-sys.path.append(os.path.join(
-        os.path.dirname(__file__),
-        "../"))
+def test_read_data(filename, delimiter=','):
+    all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=0)
 
-import src.plotting as plotting
-
-def test_read_data():
-    input_file = "gs_data.csv"
-    data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","data"))
-    input_filename = os.path.join(data_directory,input_file)
-    grainsize_data = plotting.read_data(input_filename, starting_row=0)
+    grainsize_data = np.array(all_data[1:,:], dtype=float)
+    return grainsize_data
 
     assert(grainsize_data.shape == (81,2))
     assert(grainsize_data[0,1] == 0.976563)
+
+
+def test_process_data():
+    input_data = np.array([[0,20],[1,200]])
+    function_output = plotting.process_data(input_data)
+    expected_output = np.array([[0,20,30],[1,200,310]])
+    
+    assert(np.all(function_output == expected_output))
+
+def test_plot_data():
+    plot_file = "grainsize-fig.png"
+    results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"results"))
+    plot_filename = os.path.join(results_directory,plot_file)
+
+    input_data = np.array([[0,20,30],[1,200,310]])
+    if os.path.exists(plot_filename):
+        os.remove(plot_filename)
+        
+    plotting.plot_data(input_data, plot_filename)
+
+    assert (os.path.exists(plot_filename))
+
