f97c1260839f519f88f3f21206b06478b5488793
Nicole Greco <nicolemariegreco@gmail.com>
2021-04-09 17:03:41 -0400

    Assignment 9 Final Working Tests

--- src/plotting2.py
+++ src/plotting2.py
@@ -1,45 +1,78 @@
 #!/bin/python
+"""This project reads grain size data, plots the data,
+converts the data, appends a new row, and writes the converted
+data as a .json file"""
 
 # Import the libraries we are using. It is good practice to import all necessary
 # libraries in the first lines of a file.
+import os as os
+import sys as sys
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def read_data(filename):
-    """This function reads data from a specified filename. The specified filename should point to a .csv file"""
+# Create a function to read the data file
+def read_data(filename,delimiter=',',starting_row=1):
+    """This function reads data from a specified filename.
+    The specified filename should point to a .csv file."""
     # Create an array (a multi-dimensional table) out of our data file, full of text
-    all_data = np.genfromtxt(filename, delimiter=',',skip_header=1)
-    print(all_data)
+    all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=0)
 
     # Select the data range we are interested in, convert it into a new array, full of numbers
-    grainsize_data = np.array(all_data[1:,:], dtype=float)
-    print(grainsize_data)
+    grainsize_data = np.array(all_data[starting_row:,:], dtype=float)
     return grainsize_data
-help (read_data)
-grainsize_data = read_data("../gs_data.csv")
 
-# Compute a new column by multiplying column number 1 to Kelvin
-grainsize_calculated = (grainsize_data[:,0,None] - 5) * 2
+def process_data(grainsize_data):
+    """This function takes the input data, subtracts 5 and multiplies by 2,
+    and appends a third column to the data"""
+    # Compute a new column by multiplying column number 1 to Kelvin
+    grainsize_calculated = (grainsize_data[:,0,None] - 5) * 2
+    print (grainsize_calculated)
 
-# Append this new column to the existing temperature_data array
-processed_grainsize_data = np.append(grainsize_data, grainsize_calculated,1)
-print (processed_grainsize_data)
+    # Append this new column to the existing temperature_data array
+    processed_grainsize_data = np.append(grainsize_data, grainsize_calculated,1)
+    return processed_grainsize_data
 
-# Create a figure of the processed data
-grainsize_figure = plt.figure()
-grainsize_plot = plt.plot (processed_grainsize_data[:,2],processed_grainsize_data[:,1])
-plt.xlabel("Diameter(microns)")
-plt.ylabel("Concentration")
-plt.title("Grain Size Data")
-grainsize_figure.savefig('results/grainsize-fig.png')
+def plot_data(processed_grainsize_data, plot_filename):
+    """This function plots the processed grain size data and saves the figure"""
+    # Create a figure of the processed data
+    grainsize_figure = plt.figure()
+    plt.plot (processed_grainsize_data[:,2],processed_grainsize_data[:,1])
 
+    plt.xlabel("Diameter(microns)")
+    plt.ylabel("Concentration")
+    plt.title("Grain Size Data")
 
-#all_data = pd.read_csv("gs_data.csv", index_col='Date', header=4)
-#all_data.info()
-#all_data.to_json("results/data_output.json")
+    plt.show(block=True)
+    grainsize_figure.savefig(plot_filename)
 
-#json_data = pd.read_json("results/data_output.json")
-#json_data.info()
+def convert_data(filename, output_filename):
+    """This function converts the dataset into a .json file"""
+    all_data = pd.read_csv(filename, header=0)
+    all_data.info()
+    all_data.to_json(output_filename)
+    json_data = pd.read_json(output_filename)
+    json_data.info()
 
-#print(json_data.loc['195012':'197512','Value'])
+def plot():
+    """Main program that reads a dataset, processes it, plots it,
+    and writes the converted data as a .json file"""
+    input_file = "gs_data.csv"
+    plot_file = "grainsize-fig.png"
+    json_output_file = "data_output2.json"
+
+    data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"data"))
+    results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"results"))
+    
+    input_filename = os.path.join(data_directory, input_file)
+    plot_filename = os.path.join(results_directory, plot_file)
+    json_output_file = os.path.join(results_directory, json_output_file)
+
+    grainsize_data = read_data(input_filename, starting_row=0)
+    processed_grainsize_data = process_data(grainsize_data)
+    plot_data(processed_grainsize_data, plot_filename)
+    convert_data(input_filename, json_output_file)
+
+if __name__ == "__main__":
+    print(sys.argv)
+    plot()
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,33 +1,34 @@
-import plotting2 as plotting
-import sys as sys
-import os as os
+"""This file contains tests for plotting libarary"""
+
+import sys
+import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-def test_read_data(filename, delimiter=','):
-    all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=0)
+sys.path.append(os.path.join(
+    os.path.dirname(__file__),
+    ".."))
 
-    grainsize_data = np.array(all_data[1:,:], dtype=float)
-    return grainsize_data
+from src import plotting2 as plotting
+
+def test_read_data():
+    """Test that data can be read"""
+    input_file = "gs_data.csv"
+    data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","data"))
+    input_filename = os.path.join(data_directory,input_file)
+    grainsize_data = plotting.read_data(input_filename, starting_row=1)
 
     assert(grainsize_data.shape == (81,2))
-    assert(grainsize_data[0,1] == 0.976563)
-
-
-def test_process_data():
-    input_data = np.array([[0,20],[1,200]])
-    function_output = plotting.process_data(input_data)
-    expected_output = np.array([[0,20,30],[1,200,310]])
-    
-    assert(np.all(function_output == expected_output))
+    assert(grainsize_data[0,0] == 0.976563)
 
 def test_plot_data():
+    """Test that converted data is plotted"""
     plot_file = "grainsize-fig.png"
-    results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"results"))
+    results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","results"))
     plot_filename = os.path.join(results_directory,plot_file)
 
     input_data = np.array([[0,20,30],[1,200,310]])
+    
     if os.path.exists(plot_filename):
         os.remove(plot_filename)
         
@@ -35,3 +36,20 @@ def test_plot_data():
 
     assert (os.path.exists(plot_filename))
 
+def test_convert_data():
+    """Test that data is converted"""
+    input_file = "gs_data.csv"
+    json_output_file = "data_output2.json"
+
+    data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","data"))
+    results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","results"))
+
+    input_filename = os.path.join(data_directory,input_file)
+    json_filename = os.path.join(results_directory,json_output_file)
+
+    plotting.convert_data(input_filename, json_filename)
+
+    input_data = pd.read_csv(input_filename, index_col='Microns', header=0)
+    output_data = pd.read_json(json_filename)
+
+    assert input_data.info() is output_data.info()
